b20e772e223dd815ad94f3db4e5efeb94ac53276
Lifr0m <artem.kay@yandex.ru>
2025-03-20 21:35:21 +0300

    add tests

--- tl-types/src/deserialize.rs
+++ tl-types/src/deserialize.rs
@@ -2,8 +2,6 @@ use std::io::{self, Cursor, Read};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use thiserror::Error;
 
-// todo: tests
-
 #[derive(Error, Debug)]
 pub(crate) enum Error {
     #[error("io error: {0}")]
@@ -111,5 +109,44 @@ fn deserialize_dyn_len(cur: &mut Cursor<Vec<u8>>) -> Result<usize, Error> {
         Ok(buf[0] as usize)
     } else {
         Ok(i64::deserialize(cur)? as usize)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn primitives() {
+        assert_eq!(de::<u16>(vec![0x89, 0xab]), 43913_u16);
+        assert_eq!(de::<i32>(vec![0x4e, 0x19, 0x8f, 0x1c]), 479140174_i32);
+        assert_eq!(de::<i64>(vec![0x4d, 0xbe, 0x90, 0x9, 0xa2, 0xc6, 0x35, 0x1]), 87194167051075149_i64);
+        assert_eq!(de::<f64>(vec![0xbc, 0x90, 0x0e, 0x0f, 0x61, 0x3a, 0x81, 0x40]), 551.297392_f64);
+        assert_eq!(de::<bool>(vec![0x1]), true);
+        assert_eq!(de::<bool>(vec![0x0]), false);
+        assert_eq!(de::<String>(vec![0x5, b'h', b'e', b'l', b'l', b'o']), "hello".to_string());
+        assert_eq!(
+            de::<Vec<u8>>([vec![0xFF, 0xe8, 0x3, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0], vec![0xdd; 997], vec![b'j', b'o', b'y']].concat()),
+            [vec![0xdd; 997], vec![b'j', b'o', b'y']].concat()
+        );
+        assert_eq!(de::<Option<i32>>(vec![0x1, 0x28, 0x0, 0x0, 0x0]), Some(0x28));
+        assert_eq!(de::<Option<i32>>(vec![0x0]), None::<i32>);
+    }
+
+    #[test]
+    fn dyn_len() {
+        assert_eq!(de_dyn_len(vec![0x50]), 0x50);
+        assert_eq!(de_dyn_len(vec![0xFF, 0x97, 0x43, 0x2, 0x0, 0x0, 0x0, 0x0, 0x0]), 0x24397);
+    }
+
+    fn de<T: Deserialize>(buf: Vec<u8>) -> T {
+        let mut cur = Cursor::new(buf);
+        T::deserialize(&mut cur).unwrap()
+    }
+
+    fn de_dyn_len(buf: Vec<u8>) -> usize {
+        let mut cur = Cursor::new(buf);
+        deserialize_dyn_len(&mut cur).unwrap()
     }
 }
--- tl-types/src/serialize.rs
+++ tl-types/src/serialize.rs
@@ -1,7 +1,5 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
-// todo: tests
-
 pub(crate) trait Serialize {
     fn serialize(&self, buf: &mut Vec<u8>);
 }
@@ -97,5 +95,45 @@ fn serialize_dyn_len(len: usize, buf: &mut Vec<u8>) {
     } else {
         buf.push(255);
         (len as i64).serialize(buf);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn primitives() {
+        assert_eq!(ser(43913_u16), vec![0x89, 0xab]);
+        assert_eq!(ser(479140174_i32), vec![0x4e, 0x19, 0x8f, 0x1c]);
+        assert_eq!(ser(87194167051075149_i64), vec![0x4d, 0xbe, 0x90, 0x9, 0xa2, 0xc6, 0x35, 0x1]);
+        assert_eq!(ser(551.297392_f64), vec![0xbc, 0x90, 0x0e, 0x0f, 0x61, 0x3a, 0x81, 0x40]);
+        assert_eq!(ser(true), vec![0x1]);
+        assert_eq!(ser(false), vec![0x0]);
+        assert_eq!(ser("hello".to_string()), vec![0x5, b'h', b'e', b'l', b'l', b'o']);
+        assert_eq!(
+            ser([vec![0xdd; 997], vec![b'j', b'o', b'y']].concat()),
+            [vec![0xFF, 0xe8, 0x3, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0], vec![0xdd; 997], vec![b'j', b'o', b'y']].concat()
+        );
+        assert_eq!(ser(Some(0x28)), vec![0x1, 0x28, 0x0, 0x0, 0x0]);
+        assert_eq!(ser(None::<i32>), vec![0x0]);
+    }
+
+    #[test]
+    fn dyn_len() {
+        assert_eq!(ser_dyn_len(0x50), vec![0x50]);
+        assert_eq!(ser_dyn_len(0x24397), vec![0xFF, 0x97, 0x43, 0x2, 0x0, 0x0, 0x0, 0x0, 0x0]);
+    }
+
+    fn ser<T: Serialize>(value: T) -> Vec<u8> {
+        let mut buf = Vec::<u8>::new();
+        value.serialize(&mut buf);
+        buf
+    }
+
+    fn ser_dyn_len(len: usize) -> Vec<u8> {
+        let mut buf = Vec::<u8>::new();
+        serialize_dyn_len(len, &mut buf);
+        buf
     }
 }
